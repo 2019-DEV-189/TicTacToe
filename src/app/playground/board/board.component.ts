d1b88b71596c7521fdd54710b1a0b28b2a09fa56
dev011 <aneeh.ahuja@gmail.com>
2019-09-09 01:03:27 +0530

    feat: game continues until one player has three in a row, horizontally, vertically or diagonally
    Implementation for allowing players to continue playing in alternate turns until one of them draws a three in a row horizontally, vertically or diagonally.

--- src/app/playground/board/board.component.ts
+++ src/app/playground/board/board.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { squareEnum } from '../square/squareEnum';
+import { UtilService } from '../../util/utilService';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  styleUrls: ['./board.component.css'],
+  providers:  [ UtilService ]
 })
 export class BoardComponent implements OnInit {
 
@@ -13,7 +15,10 @@ export class BoardComponent implements OnInit {
   public currentPlayer: squareEnum;
   public board: squareEnum[][];
 
-  constructor() { }
+  constructor(private utilService: UtilService) {
+    this.utilService = utilService;
+  }
+
 
   ngOnInit() {
     this.newGame();
@@ -33,9 +38,14 @@ export class BoardComponent implements OnInit {
   }
 
   updateBoard(rowIndex: number, colIndex: number): void {
-    if (this.board[rowIndex][colIndex] === squareEnum.EMPTY) {
+    if (!this.isGameOver && this.board[rowIndex][colIndex] === squareEnum.EMPTY) {
       this.board[rowIndex][colIndex] = this.currentPlayer;
-      this.nextMove();
+      if (this.isTriplet()) {
+        this.isGameOver = true;
+      }
+      else {
+        this.nextMove();
+      }
     }
   }
 
@@ -46,5 +56,42 @@ export class BoardComponent implements OnInit {
   togglePlayer(): void {
     this.currentPlayer = this.currentPlayer === squareEnum.xPlayer ? squareEnum.oPlayer : squareEnum.xPlayer;
     this.gameResultMessage = `Player ${this.currentPlayer}'s turn`;
+  }
+
+  isTriplet(): boolean {
+    return (this.isHorizontalTriplet() || this.isVerticalTriplet() || this.isDiagonalTriplet());
+  }
+
+  isHorizontalTriplet(): boolean {
+    let horizontalTriplet = false;
+    for (const colArray of this.board) {
+      if (!horizontalTriplet) {
+        horizontalTriplet = this.utilService.checkEquality(colArray[0], colArray[1], colArray[2]);
+      }
+    }
+    return horizontalTriplet;
+  }
+
+  isVerticalTriplet(): boolean {
+    let verticalTriplet = false;
+    let rowIndex = 0;
+    for (let colIndex = 0; colIndex < this.board[rowIndex].length; colIndex++) {
+      if (!verticalTriplet) {
+        verticalTriplet = this.utilService.checkEquality(this.board[0][colIndex], this.board[1][colIndex], this.board[2][colIndex]);
+      }
+    }
+    return verticalTriplet;
+  }
+
+  isDiagonalTriplet(): boolean {
+    return (this.isLeftDiagonalTriplet() || this.isRightDiagonalTriplet());
+  }
+
+  isLeftDiagonalTriplet(): boolean {
+    return this.utilService.checkEquality(this.board[0][0], this.board[1][1], this.board[2][2]);
+  }
+
+  isRightDiagonalTriplet(): boolean {
+    return this.utilService.checkEquality(this.board[0][2], this.board[1][1], this.board[2][0]);
   }
 }
