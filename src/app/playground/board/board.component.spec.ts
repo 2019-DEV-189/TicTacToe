2d565d96d1046ad24de8d79be2bfc9e0009a909d
dev011 <aneeh.ahuja@gmail.com>
2019-09-09 00:22:39 +0530

    test: X always goes first

--- src/app/playground/board/board.component.spec.ts
+++ src/app/playground/board/board.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BoardComponent } from './board.component';
+import { squareEnum } from '../square/squareEnum';
+import { SquareComponent } from '../square/square.component';
+import { By } from '@angular/platform-browser';
 
 describe('BoardComponent', () => {
   let component: BoardComponent;
   let fixture: ComponentFixture<BoardComponent>;
+  let gameStatusElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BoardComponent ]
+      declarations: [ BoardComponent,
+                      SquareComponent
+                    ]
     })
     .compileComponents();
   }));
@@ -17,9 +22,47 @@ describe('BoardComponent', () => {
     fixture = TestBed.createComponent(BoardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    gameStatusElement = fixture.debugElement.nativeElement.querySelector('#status');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('X always goes first', function() {
+    let rowIndex: number;
+    let colIndex: number;
+
+    beforeEach (function() {
+      ({ rowIndex, colIndex } = firstRuleSetup(fixture, rowIndex, colIndex, component));
+    });
+
+    it('should have X player as a current player when a new game begins', function() {
+      component.newGame();
+      expect(component.currentPlayer).toEqual(squareEnum.xPlayer);
+      expect(component.currentPlayer).not.toEqual(squareEnum.oPlayer);
+      fixture.detectChanges();
+      expect(gameStatusElement.textContent).toContain('Player X\'s turn');
+    });
+
+    it('should be X that goes in first', function() {
+      expect(component.board[rowIndex][colIndex]).toEqual(squareEnum.xPlayer);
+      expect(component.board[rowIndex][colIndex]).not.toEqual(squareEnum.oPlayer);
+    });
+  });
 });
+
+function firstRuleSetup(fixture: ComponentFixture<BoardComponent>, rowIndex: number, colIndex: number, component: BoardComponent) {
+  let squareElement = fixture.debugElement.query(By.css('app-square'));
+  rowIndex = 0;
+  colIndex = 0;
+  component.board = [
+    [squareEnum.EMPTY, squareEnum.EMPTY, squareEnum.EMPTY],
+    [squareEnum.EMPTY, squareEnum.EMPTY, squareEnum.EMPTY],
+    [squareEnum.EMPTY, squareEnum.EMPTY, squareEnum.EMPTY]
+  ];
+  squareElement.triggerEventHandler('click', null);
+  component.updateBoard(rowIndex, colIndex);
+  fixture.detectChanges();
+  return { rowIndex, colIndex };
+}
--- src/app/playground/square/square.component.spec.ts
+++ src/app/playground/square/square.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SquareComponent } from './square.component';
+import { squareEnum } from './squareEnum';
 
 describe('SquareComponent', () => {
   let component: SquareComponent;
@@ -21,5 +21,30 @@ describe('SquareComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('square has three states `EMPTY` OR `X` OR `O`', function() {
+
+    it('should have an EMPTY state initially', () => {
+      component.colIndex = 0;
+      component.rowIndex = 0;
+      expect(fixture.nativeElement.querySelector('div').innerText).toEqual(squareEnum.EMPTY);
+    });
+
+    it('should update with `X` when Player `X` makes a move', () => {
+      component.colIndex = 1;
+      component.rowIndex = 1;
+      component.squareItem = squareEnum.xPlayer;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('div').innerText).toEqual(squareEnum.xPlayer);
+    });
+
+    it('should update with `O` when Player `O` makes a move', () => {
+      component.colIndex = 2;
+      component.rowIndex = 2;
+      component.squareItem = squareEnum.oPlayer;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('div').innerText).toEqual(squareEnum.oPlayer);
+    });
   });
 });
